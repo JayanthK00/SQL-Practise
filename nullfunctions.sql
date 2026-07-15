66c0f3b587bd4410ad10bb330db09c9ca4743878
Jayanthk007 <jayanthk007cc@gmail.com>
2026-07-15 23:43:38 +0530

    null functions

--- nullfunctions.sql
+++ nullfunctions.sql
@@ -1,1 +1,42 @@
 ----------null functions-----------
+select 
+CustomerID,
+score,
+avg(score) over () avgscr,
+avg(coalesce(score,0)) over() aftercoalesce
+from sales.customers
+------------------------------------------------------
+
+select * from sales.Customers
+select 
+CustomerID,
+FirstName,
+LastName,
+firstname + ' '+LastName as fullname,
+score + 10 as incre
+from sales.Customers
+
+-----------coesce for data aggregation-----------
+select 
+CustomerID,
+FirstName,
+LastName,
+FirstName + ' '+coalesce(LastName,'') as fullname,
+score,
+coalesce(score,0) + 10 as incre
+from sales.Customers
+
+--------------coalesce for null value at last-----------
+select 
+CustomerID,
+Score,
+coalesce(score,999999999) as lazyway
+from sales.Customers
+order by lazyway asc
+
+select 
+CustomerID,
+Score,
+case when score is null then 1 else 0 end hardpro
+from sales.Customers
+order by case when score is null then 1 else 0 end 
