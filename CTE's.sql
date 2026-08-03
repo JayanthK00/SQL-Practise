2174fe2715489182ba2af177e018c352d108a8fc
Jayanthk007 <jayanthk007cc@gmail.com>
2026-08-03 23:52:00 +0530

    views

--- CTE's.sql
+++ CTE's.sql
@@ -32,22 +32,72 @@ o.CustomerID,
 max(orderdate) as lastorder
 from sales.Orders as o
 group by CustomerID)
+--mq--
 
 select
 c.CustomerID,
 c.FirstName,
 c.LastName,
 cts.totalsales,
-clo.lastorder
+ctelast.lastorder
+from sales.customers as c
+left join ctetotalsale as cts
+on cts.CustomerID=c.CustomerID
+left join cte_lastorder as ctelast
+on ctelast.CustomerID=c.CustomerID
+
+-----------------------------------------------------------
+--NESTED CTEs---------------
+WITH ctetotalsale as(
+select
+o.CustomerID,
+sum(o.sales) as totalsales
+from sales.Orders as o
+group by o.CustomerID)
+
+, cte_lastorder as(
+select 
+o.CustomerID,
+max(orderdate) as lastorder
+from sales.Orders as o
+group by CustomerID)
+
+, cte_customerank as (
+select 
+cts.CustomerID,
+cts.totalsales,
+rank() over(order by totalsales desc) ranks
+from ctetotalsale as cts)
+
+------------segmentation based on totalsales-----------
+, cteseg as (
+select 
+ctetot.customerid,
+case 
+when ctetot.totalsales > 100 then 'high'
+when ctetot.totalsales > 80 then 'mid'
+when ctetot.totalsales > 50 then 'low'
+else 'very low'
+end cteseg
+from ctetotalsale as ctetot)
+
+
+
+select
+c.CustomerID,
+c.FirstName,
+c.LastName,
+cts.totalsales,
+clo.lastorder,
+crank.ranks,
+cteseg.cteseg
 from sales.customers as c
 left join ctetotalsale as cts
 on cts.CustomerID=c.CustomerID
 left join cte_lastorder as clo
 on clo.customerid=c.CustomerID
-
-
------------------------------------------------------------
---NESTED CTEs---------------
-
-
+left join cte_customerank as crank
+on crank.CustomerID=c.CustomerID
+left join cteseg as cteseg
+on cteseg.CustomerID=c.CustomerID
 
